df70c630f73ea0b8395edc21808757ab62bc9973
kmkaze <kmilo.guitar@gmail.com>
2022-10-27 15:00:47 -0500

    first commit

---
+++ proyecto_back/models/empleados.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema  = mongoose.Schema;
+
+const EmpleadosSchema = new Schema({
+    nombre:{type: String, require: true, max:60},
+    apellido_p:{type: String, require: true, max:40},
+    apellido_m:{type: String, require: true, max:40},
+    telefono:{type: String, require: true, max:15},
+    mail:{type: String, require: false, max:70},
+    direccion:{type: String, require: false, max:150},
+
+});
+
+module.exports = mongoose.model("empleados", EmpleadosSchema);
